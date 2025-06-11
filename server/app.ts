9e62b3bb4dcf35ec7f1ab3da8cb0b4fde6f4f561
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-06-10 21:48:44 -0600

    Get tarot cards

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/cards', async (req, res) => {
+  try {
+    const response = await fetch('https://tarotapi.dev/api/v1/cards');
+    const data = await response.json();
+    res.send(data);
+  } catch {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
