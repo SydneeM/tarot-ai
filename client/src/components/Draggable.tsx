e46cfa17cedd74941bfd43010c80c800e1f81a43
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-07-06 12:20:42 -0600

    Add pentacles

--- client/src/components/Draggable.tsx
+++ client/src/components/Draggable.tsx
@@ -14,11 +14,18 @@ function Draggable({ id, dropId, children }: DraggableItemProps) {
   });
 
   const inSpread = dropId !== 'major' && dropId !== 'minor';
+  let formattedId = id;
+  if (inSpread) {
+    formattedId = id
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('');
+  }
 
   const style = {
     height: '100%',
     color: inSpread ? 'transparent' : '',
-    backgroundImage: inSpread ? `url('/${id.replace(/\s/g, '')}.png')` : '',
+    backgroundImage: inSpread ? `url('/${formattedId}.png')` : '',
     backgroundPosition: inSpread ? 'center' : '',
     backgroundSize: inSpread ? '100% 100%' : '',
     borderWidth: inSpread ? '1px' : '',
